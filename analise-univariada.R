9dfa7c5630149933ebd0f83e07013eac6522a88c
CiceroLeal <cicerorjl@hotmail.com>
2017-09-30 21:01:20 -0300

    Criado arquivo de script de analise univariada

---
+++ analise-univariada.R
@@ -0,0 +1,40 @@
+dados <- read.csv(file="headline-responses.csv", header=TRUE, sep=",")
+
+moda <- function(x) {
+  t <- table(x)
+  return(as.numeric(names(t)[t == max(t)]))
+  
+}
+
+quantitava <- function(nome, x){
+  print(nome)
+  print(paste("Media: ",mean(dados$Weightvar)))
+  print(paste("Mediana: ",median(dados$Weightvar)))
+  print(paste("Moda: ",moda(dados$Weightvar)))
+  print(paste("Desvio Padrao: ",sd(dados$Weightvar)))
+  print("")
+}
+
+qualitativa <- function(nome,x,n,...){
+  print(nome)
+  
+  #numero de observações
+  t <- length(x)
+  
+  #tipos de resposta
+  labls <- list(...)
+  
+  for(i in 1:n){
+    #frequencia da resposta
+    f = length(x[x == labls[i]])
+    
+    #porcentagem em relação ao total
+    p = round(f/t,2)*100
+    
+    print(paste(labls[i],": ",p,"%"))
+  }
+  print("")
+}
+
+quantitava('--Weightvar--',dados$Weightvar)
+qualitativa('--Recalled--',dados$recalled,3,'no', 'unsure', 'yes')
