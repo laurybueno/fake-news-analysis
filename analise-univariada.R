d2949d91f716ea7e5a46b769ff581ab84592e849
CiceroLeal <cicerorjl@hotmail.com>
2017-10-02 03:54:19 -0300

    atualização

--- analise-univariada.R
+++ analise-univariada.R
@@ -1,4 +1,5 @@
-dados <- read.csv(file="data/headline-responses.csv", header=TRUE, sep=",")
+dados <- read.csv(file="headline-responses.csv", header=TRUE, sep=",")
+pub <- read.csv(file="raw-data.csv", header=TRUE, sep=",")
 
 moda <- function(x) {
   t <- table(x)
@@ -15,11 +16,13 @@ quantitava <- function(nome, x){
   print("")
 }
 
-qualitativa <- function(nome,x,n,...){
+qualitativa <- function(nome,y,n,...){
   print(nome)
-  
+  x = y[y != ""]
+  print(y)
   #numero de observações
   t <- length(x)
+  print(paste("Total: ",t))
   
   #tipos de resposta
   labls <- list(...)
@@ -31,10 +34,14 @@ qualitativa <- function(nome,x,n,...){
     #porcentagem em relação ao total
     p = round(f/t,2)*100
     
-    print(paste(labls[i],": ",p,"%"))
+    print(paste(labls[i],": ",p,"% - ",f))
   }
   print("")
 }
 
-quantitava('--Weightvar--',dados$Weightvar)
-qualitativa('--Recalled--',dados$recalled,3,'no', 'unsure', 'yes')
+#quantitava('--Weightvar--',dados$Weightvar)
+#qualitativa('Recalled',dados$recalled,3,'no', 'unsure', 'yes')
+#qualitativa('Accuracy',dados$accuracy,4,'very accurate', 'somewhat accurate', 'not very accurate','not at all accurate')
+#qualitativa('Accuracy Bool',dados$accuracy_bool,2,'True', 'False')
+#qualitativa('Idade/genero',pub$DP_GENAGE,6,'1', '2', '3', '4', '5', '6')
+qualitativa('Partido',pub$DWD4,3,'1', '2', '3')
